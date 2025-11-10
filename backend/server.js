03ccaec4cca7e31744df1177bc371ad6f0ab61a1
PieMenard <pinedo.m.gustavo@gmail.com>
2025-11-10 17:06:40 -0400

    backend database setup

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import productRoutes from './routes/productRoutes.js';
+import { sql } from './config/db.js';
 
 dotenv.config();
 
@@ -18,8 +19,28 @@ app.use(helmet()); //helmet is a security middleware that protects the app by se
 
 app.use(morgan('dev')); //morgan logs requests
 
-app.get('/api/products', productRoutes);
+app.use('/api/products', productRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+async function initDB() {
+    try {
+        await sql`
+        CREATE TABLE IF NOT EXISTS products (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            image VARCHAR(255) NOT NULL,
+            price DECIMAL(10,2) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        `;
+
+        console.log('Database initialized successfully');
+    } catch (error) {
+        console.log('Error initalizing database');
+    }
+}
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 });
