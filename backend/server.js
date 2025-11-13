528deeb98079389e5fb3465ee0fa93aa91d79fd1
PieMenard <pinedo.m.gustavo@gmail.com>
2025-11-13 14:46:49 -0400

    preapre for render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import helmet from 'helmet';
+import helmet, { contentSecurityPolicy } from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import productRoutes from './routes/productRoutes.js';
 import { sql } from './config/db.js';
@@ -14,9 +15,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cors());
-app.use(helmet()); //helmet is a security middleware that protects the app by setting various HTTP headers
+app.use(helmet({ contentSecurityPolicy: false })); //helmet is a security middleware that protects the app by setting various HTTP headers
 
 app.use(morgan('dev')); //morgan logs requests
 
@@ -49,6 +52,15 @@ app.use(async (req, res, next) => {
 });
 
 app.use('/api/products', productRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    // serve our react app
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
 
 async function initDB() {
     try {
--- frontend/src/store/useProductStore.ts
+++ frontend/src/store/useProductStore.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import type { FormData, Product } from '../types/types';
 import { toast } from 'react-hot-toast';
 
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:3000' : '';
 
 type ProductStore = {
     products: Product[];
